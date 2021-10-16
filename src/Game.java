bf3f7541c0e4e263c0d54ab7751e6eed0ed8ba21
amitorko <amitorkofast@gmail.com>
2021-10-16 10:28:04 -0400

    Action listened

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Game {
+public class Game implements KeyListener {
     private Snake player;
     private Food food;
     private JFrame window;
@@ -18,4 +20,50 @@ public class Game {
 
     }
 
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        int keyCode =e.getKeyCode();
+        if(keyCode == KeyEvent.VK_W){
+            player.up();
+        }
+        else if(keyCode == KeyEvent.VK_S){
+            player.down();
+        }
+        else if(keyCode == KeyEvent.VK_A){
+            player.left();
+        }
+        else if(keyCode == KeyEvent.VK_D){
+            player.right();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
+
+    public Snake getPlayer(){
+        return player;
+    }
+    public void setPlayer(Snake player){
+        this.player =player;
+    }
+
+    public Food getFood() {
+        return food;
+    }
+
+    public void setFood(Food food) {
+        this.food = food;
+    }
+
+    public JFrame getWindow() {
+        return window;
+    }
+
+    public void setWindow(JFrame window) {
+        this.window = window;
+    }
 }
--- src/Snake.java
+++ src/Snake.java
@@ -7,6 +7,8 @@ public class Snake {
     private int h = Game.height;
     private int d = Game.dimension;
 
+    private String move;
+
     public Snake(){
         body = new ArrayList<>();
         Rectangle temp = new Rectangle(d,d);
@@ -20,6 +22,8 @@ public class Snake {
         temp=new Rectangle(d,d);
         temp.setLocation((w/2 -2)*d, (h/2-2)*d);
         body.add(temp);
+
+        move="NOTHING";
     }
     public ArrayList<Rectangle> getBody(){
         return body;
@@ -27,4 +31,17 @@ public class Snake {
     public void setBody(ArrayList<Rectangle> body){
         this.body=body;
     }
+    public void up(){
+        move="UP";
+    }
+    public void down(){
+        move="DOWN";
+    }
+    public void left(){
+        move="LEFT";
+    }
+    public void right(){
+        move="RIGHT";
+    }
 }
+
